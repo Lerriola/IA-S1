0f8f7bed49ffd4b509dd37a30ad4130399b9691f
Lerriola <leoarriola11@gmail.com>
2021-03-12 18:48:00 +0100

    Modified ProbRedState

--- IA/ProbRed/ProbRedState.java
+++ IA/ProbRed/ProbRedState.java
@@ -1,20 +1,41 @@
 package ProbRed;
 
+
+import IA.Red.CentrosDatos;
+import IA.Red.Sensores;
+
+import java.util.ArrayList;
+
 /**
  * Created by bejar on 17/01/17.
  */
-public class ProbRedState {
+public class ProbRedState{
+
+    // ATRIBUTES
+    static Sensores Sens;
+    static CentrosDatos Cent;
+
+    //Adjaçeny List
+    private ArrayList<Integer> DAG;
+
+    // CONSTRUCTORS
+    public ProbRedState(int nSens, int nCent, int seed) {
+        Sens = new Sensores(nSens, seed);
+        Cent = new CentrosDatos(nCent, seed);
+
+    }
+
     /* Class independent from AIMA classes
        - It has to implement the state of the problem and its operators
-     *
+    */
 
     /* State data structure
         vector with the parity of the coins (we can assume 0 = heads, 1 = tails
-     */
+    */
 
-     /* auxiliary functions */
+    /* auxiliary functions */
 
-     // Some functions will be needed for creating a copy of the state
+    // Some functions will be needed for creating a copy of the state
 
     /* ^^^^^ TO COMPLETE ^^^^^ */
 
