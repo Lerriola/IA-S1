65bedd36409404418aa31a1c426185057efc6f19
root <root@DESKTOP-MQAJNHD.localdomain>
2021-03-12 19:45:34 +0100

    inisolu

---
+++ IA/ProbRed/Main.java
@@ -0,0 +1,22 @@
+
+import ProbRed.*;
+
+import aima.search.framework.GraphSearch;
+import aima.search.framework.Problem;
+import aima.search.framework.Search;
+import aima.search.framework.SearchAgent;
+import aima.search.informed.AStarSearch;
+import aima.search.informed.IterativeDeepeningAStarSearch;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        ProbRedState pb = new ProbRedState(10, 2, 1234);
+        pb.PrintRed();
+
+    }
+}
--- IA/ProbRed/ProbRedState.java
+++ IA/ProbRed/ProbRedState.java
@@ -5,6 +5,7 @@ import IA.Red.CentrosDatos;
 import IA.Red.Sensores;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by bejar on 17/01/17.
@@ -15,14 +16,86 @@ public class ProbRedState{
     static Sensores Sens;
     static CentrosDatos Cent;
 
-    //Adjaçeny List
-    private ArrayList<Integer> DAG;
+    //Adjaceny List
+    private ArrayList<Integer> DAG; //LAS PRIMERAS POSICIONES SON SENSORES, LAS ULTIMAS SON CENTROS DE DATOS
 
     // CONSTRUCTORS
     public ProbRedState(int nSens, int nCent, int seed) {
         Sens = new Sensores(nSens, seed);
         Cent = new CentrosDatos(nCent, seed);
 
+        iniSolution();
+    }
+
+    public int SensSize(){
+        return Sens.size();
+    }
+
+    public int  CentSize(){
+        return Cent.size();
+    }
+
+    private int countCONNEX(int id) {
+        int counter = 0;
+        for (int i = 0; i < DAG.size(); ++i) {
+            if (DAG[i] == id) counter++;
+        }
+        return counter;
+    }
+
+    private int countDATA(int id){
+        double counter = 0;
+        for (int j = 0; j < SensSize(); ++j) {
+            if(j != id){
+                if(DAG[j] == id){
+                    counter += Sens[j].getCapacidad();
+                }
+            }
+        }
+        return counter;
+    }
+
+    private void iniSolution(){
+
+        DAG = new ArrayList<Integer>(SensSize() + CentSize());
+        boolean menor3 = true;
+        boolean menor25 = true;
+
+        Random rand = new Random();
+
+
+        for(int i = 0; i < SensSize(); ++i) {
+            menor3 = true;
+            menor25 = true;
+            // Generamos un int random
+            int RI = rand.nextInt(SensSize() + CentSize());
+
+            if (i != RI) {
+                if(RI < SensSize()){
+                    while(menor3) {
+                        if (countCONNEX(RI) < 3) { // Comprobamos que el sensor i no tenga más de 3 connex.
+                            DAG[i] = RI;
+                            menor3 = false
+                        } else RI = rand.nextInt(SensSize() + CentSize());
+                    }
+                }
+                else{
+                    while(menor25) {
+                        if (countCONNEX(RI) < 25) { // Comprobamos que el sensor i no tenga más de 3 connex.
+                            DAG[i] = RI;
+                            menor25 = false;
+                        }
+                        else RI =  rand.nextInt(SensSize() + CentSize());
+                    }
+                }
+            }
+        }
+    }
+
+    public void PrintRed(){
+        for(int i = 0; i < DAG.size(); ++i){
+            System.out.println(i + " apunta a " + DAG[i] + "//" );
+        }
     }
 
     /* Class independent from AIMA classes
