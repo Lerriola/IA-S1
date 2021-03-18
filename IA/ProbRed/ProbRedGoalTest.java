5e66ba5729725af74a63d33e66ace0865b528676
Lerriola <leoarriola11@gmail.com>
2021-03-18 12:13:33 +0100

    Added Initial heuristic + mod readme

--- IA/ProbRed/ProbRedGoalTest.java
+++ IA/ProbRed/ProbRedGoalTest.java
@@ -2,9 +2,6 @@ package ProbRed;
 
 import aima.search.framework.GoalTest;
 
-/**
- * Created by bejar on 17/01/17.
- */
 public class ProbRedGoalTest implements GoalTest {
 
     public boolean isGoalState(Object state){
